2ca820305dd891537dc3ba5c8e4d401ca216f829
Berk Altuğ <berkaltug96@gmail.com>
2022-02-16 23:04:04 +0300

    added captcha solver library and solved minor bugs

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ import * as DotEnv from "dotenv";
 import fs from "fs";
 import path from "path";
 DotEnv.config();
+import { names } from "./names";
+import UserAgent from "user-agents";
+import  PuppeteerExtra  from "puppeteer-extra";
+import RecaptchaPlugin from "puppeteer-extra-plugin-recaptcha";
 
+const namesArr=names
 const seed = process.env.METAMASK_MNEMONIC_PHRASE;
 const collectionName = process.env.COLLECTION_NAME;
 const createAssetURL = `https://opensea.io/collection/${collectionName}/assets/create`;
 const description = process.env.DESCRIPTION;
 const link = process.env.URL;
 const imageDir = path.join(__dirname, "images");
-
-async function connectWallet(page: Page, metamask: dappeteer.Dappeteer) {
+const captchaKey=process.env.CAPTCHAKEY
+async function connectWallet(page, metamask) {
   // OpenSea gives us a list of different wallet options. MetaMask is the first one.
   console.log("Connecting to Metamask...");
 
@@ -32,20 +37,22 @@ async function connectWallet(page: Page, metamask: dappeteer.Dappeteer) {
   console.log("Wallet connected");
 }
 
-async function uploadImage(page: Page, filepath: string) {
+async function uploadImage(page, filepath) {
   const elementHandle = await page.$("#media");
   await elementHandle.uploadFile(filepath);
 }
 
 async function fillFields(
-  page: Page,
-  name: string,
-  description: string,
-  link: string
+  page,
+  name,
+  description,
+  link
 ) {
   // Get and fill in the input name
   await page.focus("#name");
-  await page.keyboard.type(name, { delay: 25 });
+  let newname=namesArr[0] + "'s Breasts"
+  await page.keyboard.type(newname, { delay: 25 });
+  namesArr.shift()
 
   // Get and fill in the description
   await page.focus("#description");
@@ -55,9 +62,21 @@ async function fillFields(
   await page.keyboard.type(link, { delay: 25 });
 }
 
+
 async function main() {
   // Launch the browser with MetaMask
-  const browser = await dappeteer.launch(puppeteer, {
+ 
+  PuppeteerExtra.use(
+    RecaptchaPlugin({
+      provider: {
+        id: '2captcha',
+        token: captchaKey // REPLACE THIS WITH YOUR OWN 2CAPTCHA API KEY ⚡
+      },
+      visualFeedback: true // colorize reCAPTCHAs (violet = detected, green = solved)
+    })
+  )
+//@ts-ignore
+  const browser = await dappeteer.launch(PuppeteerExtra, {
     metamaskVersion: "v10.1.1",
   });
   const metamask = await dappeteer.setupMetamask(browser, { seed });
@@ -65,26 +84,32 @@ async function main() {
   // Open OpenSea.io website in a new tab
   console.log("Launching OpenSea...");
   const page = await browser.newPage();
+  const userAgent=new UserAgent();
+  await page.setUserAgent(userAgent.toString());
+  await page.setDefaultNavigationTimeout(0);
+  await page.setDefaultTimeout(0);
+
   await page.goto(createAssetURL, { waitUntil: "networkidle0" });
   await page.bringToFront();
-
+  
   // Close first empty tab
   const tabs = await browser.pages();
   await tabs[0].close();
-
+  
   // Connect OpenSea with MetaMask
   await connectWallet(page, metamask);
   await page.waitForTimeout(2000);
-
+  
   // The first request will need to be signed explicitly
   console.log("Sign initial request...");
   await metamask.sign();
   await page.bringToFront();
   await page.waitForTimeout(2000);
-
+  
   // Read the contents of the images folder
   const files = fs.readdirSync(imageDir);
-
+  
+ 
   // Start the loop on each image of images folder
   for (const file of files) {
     if (file === ".DS_Store") {
@@ -111,6 +136,8 @@ async function main() {
 
     // Now wait for the success popup to appear
     console.log("Waiting for success popup...");
+    await page.solveRecaptchas()
+    
     await page.waitForSelector(
       "div.AssetSuccessModalContentreact__DivContainer-sc-1vt1rp8-1"
     );
